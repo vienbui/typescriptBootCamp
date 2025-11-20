d77f4c543a3f88d30660a0ac35c584003fc3e888
vbui <vbui@axon.com>
2025-11-20 15:31:23 +0700

    add less19

--- fundamentals/03-null-undefined-changing.js
+++ fundamentals/03-null-undefined-changing.js
@@ -1,8 +1,9 @@
-var course = {
-    title: "TS Bootcamp"
-};
-var title = null;
-console.log("title =" + title);
-if (course.title) {
-    console.log("Title is ".concat(course.title));
+var _a, _b, _c;
+var course = null;
+// const title = course.textField?.title ?? "No Title Found";
+// console.log("title =" + title);
+var title = (_b = (_a = course === null || course === void 0 ? void 0 : course.textField) === null || _a === void 0 ? void 0 : _a.title) !== null && _b !== void 0 ? _b : "No Title Found";
+console.log(title);
+if ((_c = course === null || course === void 0 ? void 0 : course.textField) === null || _c === void 0 ? void 0 : _c.title) {
+    console.log("Title is ".concat(course.textField.title));
 }
