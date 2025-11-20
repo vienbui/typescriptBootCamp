54e66bfe3a9b6c5a5aa00bfd57ac884686b800a0
vbui <vbui@axon.com>
2025-11-20 16:27:24 +0700

    add less23 avoid implicit any type

---
+++ fundamentals/06-any-types.ts
@@ -0,0 +1,10 @@
+
+let lessonCount:any = 10;
+
+let number:any[] = [1,2,3, "Hello", true];
+
+printCourse(10, "Typescript Basics");
+
+function printCourse(title:string, lessonCount){
+    console.log(`Course ${title} has ${lessonCount} lessons.`);
+}
