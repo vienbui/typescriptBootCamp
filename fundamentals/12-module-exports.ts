b235c644cb276744dd10dbb04861ea2726990832
vbui <vbui@axon.com>
2025-11-24 15:57:15 +0700

    add less 32, 12-module

---
+++ fundamentals/12-module-exports.ts
@@ -0,0 +1,20 @@
+
+
+export {};
+
+export type Course = {
+    title: string;
+    subtitle: string;
+    lessonCount: number;
+}
+
+export const PAGE_SIZE = 100;
+
+const pagesize = PAGE_SIZE;
+
+const COURSE ={
+    title: "Typescript Modules",
+    subtitle: "Learn about modules in Typescript",
+    lessonCount: 10
+
+}
---
+++ fundamentals/12-module-imports.ts
@@ -0,0 +1,5 @@
+
+import {PAGE_SIZE, COURSE, Course} from "./12-module-exports";
+
+const pageSize = PAGE_SIZE;
+
