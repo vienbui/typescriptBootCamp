930d90ebc0954bdf0309c8bb8d41a7637cf5c61d
vbui <vbui@axon.com>
2025-12-02 16:47:20 +0700

    less73 inhenrit

--- classes/01-classes-intro.ts
+++ classes/01-classes-intro.ts
@@ -1,17 +1,30 @@
 
 class Course{
 
-   static TOTAL_COURSES = 0;
+   private static TOTAL_COURSES = 0;
+   static readonly TYPESCRIPT_TITLE = "TypeScript Basics";
 
     constructor(
        private _title:string,
-        private subtitle:string,
-       private creationDate: Date
+       private price:number,
+        private subtitle = "",
+       private creationDate = new Date (2025,11,1)
     ) {
 
+        this.validatePrice();
         Course.TOTAL_COURSES++;
        }
     
+       validatePrice(){
+        console.log("Called Course validatiePrice ()");
+        if(this.price <= 0){
+            throw "Price must be larger than 0"
+        }
+       }
+
+       static printTitle(course: Course){
+        console.log(`This course title is: ${course._title}`);
+       }
 
     get title(){
         return this._title;
@@ -30,19 +43,33 @@ class Course{
     }
 
 }
-const typeScript = new Course(
-        "TypeScript Basics", 
-        "Learn the basics of TypeScript", 
-        new Date(2025,11,2));
 
-const angular = new Course(
-    "Angular Basics", 
-    "Learn the basics of Angular", 
-    new Date(2025,10,15));
+class FreeCourse extends Course {
+    constructor(
+        _title:string,
+         subtitle = "",
+        creationDate = new Date (2025,11,1)){
+        super(_title,0,subtitle,creationDate);
+       }
+       validatePrice(){
+                console.log("Called FreeCourse validatiePrice ()");
+       }
+
+    }
+
+const typeScript = new Course(Course.TYPESCRIPT_TITLE, 100);
+
+const angular = new FreeCourse(
+    "Angular Basics" 
+    );
 
     
   
 console.log(typeScript.title);
 console.log(angular.title);
-console.log(`Total courses: ${Course.TOTAL_COURSES}`);
+// console.log(`Total courses: ${Course.TOTAL_COURSES}`);
+console.log(angular)
+
+
+
 
