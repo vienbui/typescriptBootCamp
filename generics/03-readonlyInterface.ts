cfc2d5e5a7389505a1c93dbd75f9c8bad766ad56
vbui <vbui@axon.com>
2025-12-03 15:03:38 +0700

    OptionalAndReadOnlyGEneric

---
+++ generics/03-readonlyInterface.ts
@@ -0,0 +1,23 @@
+interface Course {
+    title:string;
+    subtitle:string;
+    lessonsCount:number;  
+}
+
+// interface InmutableCourse {
+//     readonly title:string;
+//     readonly subtitle:string;
+//     readonly lessonsCount:number;  
+// }
+
+function freezeCourse(course:Course): Readonly<Course>{
+    return Object.freeze(course);
+}
+
+const frozen = freezeCourse({
+    title: "Introduction to TypeScript",
+    subtitle: "Learn the basics of TypeScript",
+    lessonsCount: 100
+})
+
+frozen.title = "New title"; // Error: Cannot assign to 'title' because it is a read-only property.
