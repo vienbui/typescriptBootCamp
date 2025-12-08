958bf088dc165b0096c77e3f970ecd595c920982
vbui <vbui@axon.com>
2025-12-08 15:55:01 +0700

    KeyGeneric

--- generics/04-generic-functions.ts
+++ generics/04-generic-functions.ts
@@ -9,11 +9,28 @@ export function freezeCourse(course:Course): Readonly<Course>{
     return Object.freeze(course);
 }
 
+function freezeLesson(lesson:Lesson): Readonly<Lesson>{
+    return Object.freeze(lesson);
+}
+
+function freeze<T> (input: T): Readonly<T>{
+    return Object.freeze(input);
+}
+
+const course: Course = {
+ title: "Introduction to TypeScript",
+    subtitle: "Learn the basics of TypeScript",
+    lessonsCount: 100
+}
+
+const frozenCourse = freeze(course)
+
 interface Lesson {
     title:string;
     seqNo:number
 }
 
-function freezeLesson(lesson:Lesson): Readonly<Lesson>{
-    return Object.freeze(lesson);
-}
+const frozenLesson = freeze<Lesson>({
+    title: "Lesson 1: Introduction",
+    seqNo: 10
+})
