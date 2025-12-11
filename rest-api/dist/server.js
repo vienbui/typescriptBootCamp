8576387562164d96080aa3fcc233cb13544e16dd
vbui <vbui@axon.com>
2025-12-12 00:45:30 +0700

    setup server reload automatically

--- rest-api/dist/server.js
+++ rest-api/dist/server.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var root_1 = require("./routes/root");
+var app = express();
+function setupExpress() {
+    app.route("/").get(root_1.root);
+}
+function startServer() {
+    app.listen(9000, function () {
+        console.log('v1 HTTP REST API server is now running at http://localhost:9000');
+    });
+}
+;
+setupExpress();
+startServer();
--- rest-api/src/server.ts
+++ rest-api/src/server.ts
@@ -1,2 +1,22 @@
 
-console.log ("Hello world")
+import * as express from 'express';
+import { root } from './routes/root';
+
+const app = express();
+
+function setupExpress(){
+    app.route ("/").get(root);
+}
+
+function startServer() {
+
+    app.listen(9000, () => {
+
+        console.log('v1 HTTP REST API server is now running at http://localhost:9000')
+    })
+};
+
+setupExpress();
+
+
+startServer();
