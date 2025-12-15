56e599809f1ea51e2fa5d9ad4e060ecc19a60f60
vbui <vbui@axon.com>
2025-12-16 01:20:57 +0700

    less 116 CROS

--- rest-api/src/routes/get-all-course.ts
+++ rest-api/src/routes/get-all-course.ts
@@ -9,7 +9,7 @@ const router = Router();
 export const getAllCourses = catchAsync(async (request: Request, response: Response, next: NextFunction) => {
   
         // Truy vấn lấy dữ liệu từ bảng Course, sắp xếp theo seqNo
-        const result = await pool.query('SELECT * FROM "Course123" ORDER BY "seqNo"');
+        const result = await pool.query('SELECT * FROM "Course" ORDER BY "seqNo"');
 
         // Trả dữ liệu về dạng JSON
         response.status(200).json({ payload: result.rows });
